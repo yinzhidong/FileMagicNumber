210c3a9a8a1cc20987af32505d6e9bfafdb774fb
mateusz <kucka.mateusz14@gmail.com>
2019-02-26 00:08:22 +0100

    clean up

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ public class App {
 
     public static void main(String [] args) {
 
-        FileProcessor processor = new FileProcessor();
-            processor.processFile();
+        new FileProcessor().processFile();
     }
 }
